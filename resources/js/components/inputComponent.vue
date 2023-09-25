40a47e1a4c23ef4d267036719cc4618ecdb77399
yamanaka-kazuki <kazuki.yamanaka0013@gmail.com>
2023-09-25 20:54:04 +0900

    input-componentの雛形完成

---
+++ resources/js/components/inputComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <form class="d-flex justify-content-center mt-4">
+            <div class="form-group">
+                <input
+                    type="text"
+                    class="form-control form-control-lg"
+                    id="title"
+                    placeholder="目標を入力"
+                />
+            </div>
+            <div class="form-group">
+                <input
+                    type="date"
+                    class="form-control form-control-lg"
+                    id="deadline"
+                    placeholder="日付を選択"
+                />
+            </div>
+            <button type="button" class="btn btn-outline-dark">追加</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#title {
+    width: 450px;
+}
+.btn {
+    height: 45px;
+}
+</style>
